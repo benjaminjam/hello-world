2808a7ffa11a655ec511ec1334e54522e6d9900c
benjaminjam <47434974+benjaminjam@users.noreply.github.com>
2019-02-08 13:31:12 -0500

    Add files via upload

---
+++ src/githubhelloworld/GitHubHelloWorld.java
@@ -0,0 +1,16 @@
+
+package githubhelloworld;
+
+
+public class GitHubHelloWorld {
+
+    public static void main(String[] args)
+    {
+        display();
+    }
+    public static void display()
+    {
+        System.out.println("Benjamin Diehl!!!!");
+    }
+    
+}
